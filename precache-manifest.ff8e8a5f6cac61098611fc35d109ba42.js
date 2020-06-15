faa15d948a667b47c82689896cac9ca5a230629b
Lawrence Wu <llwu@llwu.me>
2020-06-15 00:08:24 -0700

    Updates

---
+++ precache-manifest.ff8e8a5f6cac61098611fc35d109ba42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1772efd5a6c72e7a5839b54867beb23",
+    "url": "/bac-volunteer-cms/index.html"
+  },
+  {
+    "revision": "62469ee626a2538efc4b",
+    "url": "/bac-volunteer-cms/static/css/main.3fd04b9d.chunk.css"
+  },
+  {
+    "revision": "4e1a789aecbaed9f715c",
+    "url": "/bac-volunteer-cms/static/js/2.9a5e2994.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/bac-volunteer-cms/static/js/2.9a5e2994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62469ee626a2538efc4b",
+    "url": "/bac-volunteer-cms/static/js/main.14f8460e.chunk.js"
+  },
+  {
+    "revision": "1987d3fd63545af74621",
+    "url": "/bac-volunteer-cms/static/js/runtime-main.d4c9100f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d0d8a8d6e625a5cdf6f5dcd1fb57f79.js"
+  "/bac-volunteer-cms/precache-manifest.ff8e8a5f6cac61098611fc35d109ba42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bac-volunteer-cms/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
