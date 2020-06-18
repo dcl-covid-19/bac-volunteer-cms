b86d3c2300ebee4682b10e6ac03c04a6bfa2594a
Lawrence Wu <llwu@llwu.me>
2020-06-17 21:07:31 -0700

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import App from './App';
+import "typeface-roboto";
+
+const theme = createMuiTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <MuiThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
+        <App />
+      </React.Fragment>
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
